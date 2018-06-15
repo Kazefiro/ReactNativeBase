667e5643799f95f6dc7905a84a0b2a9f790cd667
c1256175 <>
2018-06-14 22:33:19 -0300

    Aula 02

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import { AppRegistry } from 'react-native';
 import App from './src/App';
+import Login from './src/screens/Login';
+import Register from './src/screens/Register';
 
-AppRegistry.registerComponent('ToDoManager_TurmaA_2017', () => App);
+AppRegistry
+    .registerComponent('ToDoManager_TurmaA_2017', () => Register);
