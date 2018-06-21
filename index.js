3d76912e3d788e17b570a507f3bd17d8f7c697a0
c1256175 <>
2018-06-20 23:07:47 -0300

    Aula_02

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import { AppRegistry } from 'react-native';
-import App from './src/App';
 import Login from './src/screens/Login';
 import Register from './src/screens/Register';
 
-AppRegistry
-    .registerComponent('ToDoManager_TurmaA_2017', () => Register);
+AppRegistry.registerComponent('ToDoManager_TurmaA_2017', () => Register);
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
-import {
-    KeyboardAvoidingView, StyleSheet, View,
-    Image, TextInput, Button, Text, SafeAreaView, Alert
-} from 'react-native';
+import { SafeAreaView, KeyboardAvoidingView, StyleSheet, View, Image, TextInput, Button, Text, Alert } from 'react-native';
 
 const img = require('../assets/TodoList.png');
 
@@ -22,37 +19,38 @@ export default class Login extends Component {
 
     render() {
         return (
-            //<SafeAreaView style={styles.container}>
-            <KeyboardAvoidingView style={styles.container}
-                behavior='padding'>
-                <View style={styles.topView}>
-                    <Image style={styles.img} source={img} />
-                </View>
-                <View style={styles.bottomView}>
-                    <TextInput style={styles.input}
-                        value={this.state.email}
-                        placeholder='Email'
-                        keyboardType={'email-address'}
-                        autoCapitalize='none'
-                        onChangeText={(text) => this.setState({ email: text })} />
-                    <TextInput style={styles.input}
-                        placeholder='Password'
-                        secureTextEntry={true}
-                        onChangeText={(password) => this.setState({ password })} />
-                    <Button title='Sign In'
-                        onPress={() => Alert.alert(`Email: ${this.state.email}\nPassword: ${this.state.password}`)} />
-                    <View style={styles.textConteiner}>
-                        <Text>Not a member? Let's </Text>
-                        <Text style={styles.textRegister}>
-                            Register
-                        </Text>
+            <SafeAreaView style={{ flex: 1 }}>
+                <KeyboardAvoidingView style={styles.container}
+                    behavior='padding'>
+                    <View style={styles.topView}>
+                        <Image style={styles.img} source={img} />
                     </View>
-                </View>
-            </KeyboardAvoidingView>
-            //</SafeAreaView>
+                    <View style={styles.bottomView}>
+                        <TextInput style={styles.input}
+                            value={this.state.email}
+                            placeholder='Email'
+                            keyboardType={'email-address'}
+                            autoCapitalize='none'
+                            onChangeText={(text) => this.setState({ email: text })} />
+                        <TextInput style={styles.input}
+                            placeholder='Password'
+                            secureTextEntry={true}
+                            onChangeText={(password) => this.setState({ password })} />
+                        <Button title='Sign In'
+                            onPress={() => Alert.alert(`Email: ${this.state.email}\nPassword: ${this.state.password}`)} />
+                        <View style={styles.textConteiner}>
+                            <Text>Not a member? Let's </Text>
+                            <Text style={styles.textRegister}>
+                                Register
+                            </Text>
+                        </View>
+                    </View>
+                </KeyboardAvoidingView>
+            </SafeAreaView>
         );
     }
 }
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
